e15121c707799b5dfb7831111646750ed0aff385
Roei Levi <roei@example.com>
2019-04-09 02:01:50 +0300

    Adding server app

--- run.py
+++ run.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
+from special_report.app.app import app
+
+HOST = "localhost"
+PORT = 8080
+
+
 def main():
     print("Out first project!")
+    app.run(host=HOST, port=PORT, debug=True)
+
+    while True:
+        pass
 
 
 if __name__ == '__main__':
---
+++ special_report/app/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+
+import urllib.request as urllib
+import base64
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify('Index Page')
+
+@app.route('/hello')
+def hello():
+    return jsonify('Hello, World')
+
+def fill_in_data(data, addr):
+    with open('imageToSave_' + addr + '.png', 'wb') as fh:
+        fh.write(base64.decodebytes(data))
+    return 'goodbye'
+
+def show_the_dog():
+    img_url = 'https://www.pythonanywhere.com/user/SpecialReport/files/home/SpecialReport/imageToSave_10.0.0.162.png'
+    return render_template("index.html", user_image = img_url)
+
+@app.route('/photo', methods=['GET', 'POST'])
+def photo():
+    if request.method == 'POST':
+        return fill_in_data(request.data, request.remote_addr)
+    else:
+        return show_the_dog()
